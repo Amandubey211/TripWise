74cd7dbf889690d3a1e81d9fc55ba50bf82d10ad
Amandubey211 <109097090+Amandubey211@users.noreply.github.com>
2024-01-31 12:13:13 +0530

    initilaized app with tailwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2 className="bg-red-500 text-4xl text-center my-48">tailwind is working</h2>
     </div>
   );
-}
+};
 
 export default App;
